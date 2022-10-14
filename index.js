ff03b3af7f799da040b6148cba37f7a9a34b442e
Juan Sebastian <juaaley250@gmail.com>
2022-10-14 14:25:21 -0500

    comit inicial

---
+++ index.js
@@ -0,0 +1,74 @@
+//console.log("holamundo");
+
+
+//var miVariable= 24;
+// console.log ("mi edad es " + miVariable);
+
+//miVariable = 12 ;
+// console.log ("mi edad ahora es "+ miVariable);
+
+
+//var opcion1 = 10;
+//var opcion2 = 20;
+//var opcion3 = 30;
+
+
+//var resultado = opcion1+ opcion2+ opcion3 ;
+//console.log (resultado + 40);
+
+//let miNumero = 6;
+//let miNombre ="sebastian";
+
+//let resultadoPregunta = miNumero== 7; 
+//console.log (resultadoPregunta);
+
+ //if (miNumero <6 || miNombre == "david "  ){
+  //  console.log  ("si ") 
+//}else {
+  //  console.log ("No")
+//}
+ 
+//var miNuemero2 = 5;
+
+//if (miNumero2 > 0 ) {
+  //  console.log ('Mi numero es positivo') ;
+//}else if (miNumero2 === 0) {
+  //  console.log ('Mi numero es zero ');
+//}else{
+  //  console.log ('mi numero es negativo ');
+
+//}
+
+//let contador= 0;
+//while (contador <10 ){
+   // console.log (contador)
+
+  //  contador = contador + 1 
+//}
+//for(contador = 0 ; contador< 10; contador = contador + 1 ){
+  //  console.log ('contador')
+//}
+
+function saludar (nombre, edad){
+    console.log ('Hola Mi nombre es' + nombre);
+    console.log ('hola mi edad es'  +   edad );
+}
+
+saludar("david",24 );
+
+function multiplicar (num1, num2){
+    let resultado = num1 * num2 ;
+    return resultado ;
+}
+
+console.log (multiplicar (2, 5));
+    
+
+let miArreglo = ['david','fernando','kevin','juan'];
+
+let mostrar = miArreglo[0];
+conosole.log(mostrar);
+
+
+for(let contador= 0 ; contador <4 ; contador++ ){
+  }
